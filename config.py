2d23fc2ffa0d46c247b3e1c55493928373ce5693
PavelNikolaev <kartaxel1488@gmail.com>
2024-02-07 20:49:45 +0300

    empty token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTE4NjkxNTc4NDEzMjQxMTQ1Mg.GA0GMg.TrHIAp-joT00wexFrvpw1uwriWpxZkSgt4o5Ks',
+    'token': '',
     'bot': 'PlayerRegister',
     'server_id': 1048904315986772039
 }
